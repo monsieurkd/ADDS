484779829391301837644d353c0a1e3a0916dbb4
monsieurkd <87173358+monsieurkd@users.noreply.github.com>
2024-03-02 12:12:23 +1030

    d

---
+++ single_reponsibility_principle/main.cpp
@@ -0,0 +1,18 @@
+// each class has one reason to change
+class Calculator
+{
+    int multiplication(int a, int b);
+    int plus(int a, int b);
+    int subtract(int a, int b);
+    int divine(int a, int b);
+
+
+};
+
+class TransferData{
+    void transferDataToUser();
+
+};
+
+// and so on
+
