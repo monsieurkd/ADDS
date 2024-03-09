d97e1b6de2659070b6ef53b613637bfb8f2ae864
monsieurkd <87173358+monsieurkd@users.noreply.github.com>
2024-03-10 06:13:10 +1030

    d

--- Move.h
+++ Move.h
@@ -1,75 +1,125 @@
 #ifndef MOVE_H
 #define MOVE_H
 #include <string>
+#include <vector>
 class Move
 {
 protected:
     std::string move;
-
+    vector<string> winHand;
 public:
     Move();
     Move(std::string m);
     std::string getName();
+    vector<string> getVector();
 };
+
+
+
+
+
+
 
 class Monkey : public Move
 {
+    protected:
+        vector<string> winHand = {"Ninja",
+                             "Robot"};
 public:
+vector<string> getVector(){
+    return this->winHand;
+}
     Monkey() : Move("Monkey")
     {
     }
 };
 
 class Robot : public Move
-{
+{protected:
+        vector<string> winHand = {"Ninja",
+                            "Zombie"};
 public:
+vector<string> getVector(){
+    return this->winHand;
+}
     Robot() : Move("Robot")
     {
     }
 };
 class Ninja : public Move
-{
+{   protected:
+     vector<string> winHand= {"Pirate",
+                            "Zombie"};
 public:
+vector<string> getVector(){
+    return this->winHand;
+}
     Ninja() : Move("Ninja")
     {
     }
 };
 class Pirate : public Move
-{
+{protected:
+        vector<string> winHand= {"Robot",
+                             "Monkey"};
 public:
+vector<string> getVector(){
+    return this->winHand;
+}
     Pirate() : Move("Pirate")
     {
     }
 };
+
 class Zombie : public Move
-{
+{protected:
+        vector<string> winHand = {"Pirate",
+                             "Monkey"};
 public:
+vector<string> getVector(){
+    return this->winHand;
+}
     Zombie() : Move("Zombie")
     {
     }
 };
 class Rock : public Move
-{
+{protected:
+    vector<string> winHand = {"Scissors"};
+
 public:
+vector<string> getVector(){
+    return this->winHand;
+}
     Rock() : Move("Rock")
     {
     }
 };
 class Paper : public Move
-{
+{protected:
+    vector<string> winHand = {"Rock"};
+
 public:
+vector<string> getVector(){
+    return this->winHand;
+}
     Paper() : Move("Paper")
     {
     }
 };class Scissors : public Move
-{
+{protected:
+    vector<string> winHand = {"Paper"};
+
 public:
+vector<string> getVector(){
+    return this->winHand;
+}
     Scissors() : Move("Scissors")
     {
     }
 };
 class Assign: public Move
-{
+{protected:
     Move *move;
 
 public:
--- Referee.cpp
+++ Referee.cpp
@@ -12,73 +12,31 @@ Referee::Referee(){}; // constructor
 Player *Referee::refGame(Player *player1, Player *player2)
 {
     Move *a = player1->makeMove();
-    string p1 = player1->getName();
     Move *b = player2->makeMove();
+    string  result = a->getName();
+    string target = a->getName();
+    
+    vector<string> winHandA = a->getVector();
+    string p1 = player1->getName();
+
     string p2 = player2->getName();
 
    
 
-    vector<string> Robot = {"Ninja",
-                            "Zombie"};
-    vector<string> Monkey = {"Ninja",
-                             "Robot"};
-    vector<string> Pirate = {"Robot",
-                             "Monkey"};
-    vector<string> Ninja = {"Pirate",
-                            "Zombie"};
-    vector<string> Zombie = {"Pirate",
-                             "Monkey"};
-    vector<string> Rock = {"Scissors"};
-    vector<string> Paper = {"Rock"};
-    vector<string> Scissors = {"Paper"};
+
 
     if (p1 == p2)
     {
         return nullptr;
     }
-    else if ((p1 == "Ninja") && (find(Ninja.begin(), Ninja.end(), p2) != Ninja.end()))
-    {
 
-        return player1;
+    for (int i = 0; i <winHandA.size(); i++){
+        if(winHandA[i]==target){
+            return player1;
     }
-    else if ((p1 == "Robot") && (find(Robot.begin(), Robot.end(), p2) != Robot.end()))
-    {
+    
 
-        return player1;
-    }
-    else if ((p1 == "Monkey") && (find(Monkey.begin(), Monkey.end(), p2) != Monkey.end()))
-    {
-
-        return player1;
-    }
-    else if ((p1 == "Pirate") && (find(Pirate.begin(), Pirate.end(), p2) != Pirate.end()))
-    {
-
-        return player1;
-    }
-    else if ((p1 == "Zombie") && (find(Zombie.begin(), Zombie.end(), p2) != Zombie.end()))
-    {
-
-        return player1;
-    }
-    else if ((p1 == "Rock") && (find(Rock.begin(), Rock.end(), p2) != Rock.end()))
-    {
-
-        return player1;
-    }
-    else if ((p1 == "Paper") && (find(Paper.begin(), Paper.end(), p2) != Paper.end()))
-    {
-
-        return player1;
-    }
-    else if ((p1 == "Scissors") && (find(Scissors.begin(), Scissors.end(), p2) != Scissors.end()))
-    {
-
-        return player1;
-    }
-    else
-    {
         return player2;
-    }
+    
     // player2 wins
 };
